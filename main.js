46efed1350fae45a566082d7339765ee62136504
Leonardo Valdes <leon_valdes7591@yahoo.com>
2023-06-12 21:31:13 -0700

    add basic operation function

--- main.js
+++ main.js
@@ -13,3 +13,26 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
 	return num1 / num2;
 }
+
+function operate(num1, num2, operator) {
+	let answer;
+	switch (operator) {
+		case '+':
+			answer = add(num1, num2);
+			break;
+		case '-':
+			answer = subtract(num1, num2);
+			break;
+		case '*':
+			answer = multiply(num1, num2);
+			break;
+		case '/':
+			answer = divide(num1, num2);
+			break;
+	}
+	return answer;
+}
+
+const firstNumber = document.querySelector('.first-number');
+const secondNumber = document.querySelector('.second-number');
+const operator = document.querySelector('.operator');
